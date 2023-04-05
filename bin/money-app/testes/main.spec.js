2cb641795bb47db3fbc938eb4aecba58279ced66
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-05 10:49:51 -0300

    melhor

--- bin/money-app/testes/main.spec.js
+++ bin/money-app/testes/main.spec.js
@@ -1,8 +1,11 @@
-import chai from "chai";
+import chai, { expect } from "chai";
+import { stdout } from "node:process";
 import { describe } from "node:test";
 const exec=require(`child.process`)
 const btcconverter=`../main.js`
 describe(`main cli`,()=>{
-exec(btcconverter,()=>{})
+exec(btcconverter,(err,stdout,stderr)=>{})
+if(err) throw err
+expect (stdout).to.be.equal("hallo word")
 
 })
