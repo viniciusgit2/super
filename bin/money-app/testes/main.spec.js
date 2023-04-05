89a2956ec9f784e19814f0adeb4133a0bdf47c48
viniciusgit2 <4xv6patelfin@gmail.com>
2023-04-05 10:44:12 -0300

    melhor

---
+++ bin/money-app/testes/main.spec.js
@@ -0,0 +1,8 @@
+import chai from "chai";
+import { describe } from "node:test";
+const exec=require(`child.process`)
+const btcconverter=`../main.js`
+describe(`main cli`,()=>{
+exec(btcconverter,()=>{})
+
+})
